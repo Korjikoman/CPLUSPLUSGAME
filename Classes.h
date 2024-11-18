ae819f0b7f4d48b71f2fcf5a16d6ff60bb81d19d
Korjik <showmasniok@gmail.com>
2024-11-18 16:26:51 +0700

    Continue transforming structures to classes

--- Classes.h
+++ Classes.h
@@ -8,93 +8,113 @@ private:
     int current_health;
     int max_health;
 public:
-    int getCurrentHealth() const { return current_health; }
-    int getMaxHealth() const { return max_health; }
+    Health(int current, int max);
+    int getCurrentHealth() const;
+    int getMaxHealth() const;
 };
 
-struct Potion // структура для представления зелий восстановления
+class Potion // структура для представления зелий восстановления
 {
-    int x, y;
-    int health_restore;
-    bool collected;
+    private:
+        int x, y;
+        int health_restore;
+        bool collected;
+    public:
+    Potion(int px, int py, int restore);
+
+    bool isCollected() const;
+    void collect();
+    void print_potion();
+    int getRestoreAmount();
+    int getX();
+    int getY();
+
 };
 
-struct Coin // структура для представления монеток
+class Coin // структура для представления монеток
 {
-    int x, y;
-    int value;
-    bool collected;
+    private:
+        int x, y;
+        int value;
+        bool collected;
+    public:
+    int getX();
+    int getY();
+    int getValue();
+    Coin(int px, int py);
+    void print_coin();
+    void collect_coin(Player& player);
+
 };
 
-struct Inventory // структура для представления инвенторя игрока
+class Inventory // структура для представления инвенторя игрока
 {
-    int space; 
-    int items_count;
-    int current_element;
-    int current_space;
-    struct Item **inventory_items;
+    private:
+        int space; 
+        int items_count;
+        int current_element;
+        int current_space;
+        struct Item **inventory_items;
+
+    public: 
+    Inventory(int space); // инициализация
+    
+    void print_inventory(); // вывод инвентаря
+    int inventory_add_item(Item& item); 
 };
 
-struct Monsters // структура для представления монстров
+class Monsters // структура для представления монстров
 {
-    int x, y;
-    int damage;
-    Health health;
-    bool is_alive;
+    private:
+        int x, y;
+        int damage;
+        Health health;
+        bool is_alive;
+    public:
+
+Monsters(int x, int y, int damage, int health); // инициализация
+void print_monster(); // вывод характеристик монстра
+void monster_attack(Player& player); // монстр атакует
+void damage_monster(int amount);
+
+
 };
 
-struct Player // структура для представления игрока
+class Player // структура для представления игрока
 {
-    int x, y;
-    Health health;
-    int speed;
-    int damage;
-    Inventory inventory;
-    Potion *potions;
-    bool is_alive;
-    int coins;
+    private:
+        int x, y;
+        Health health;
+        int speed;
+        int damage;
+        Inventory inventory;
+        Potion *potions;
+        bool is_alive;
+        int coins;
+    public:
+
+    Player(int x, int y, int health, int speed, int damage, int inventory_space); // инициализация
+void print_player(); // вывод игрока
+void move_player(int dx, int dy); // перемещаем игрока
+void heal_player( int heal); // лечим игрока
+bool player_is_dead(); // проверяем, умер ли игрок
+void damage_player(int damage); // наносим урон игроку 
+
+
 };
 
-struct Item // структура для представления оружия
+class Item // структура для представления оружия
 {
-    const char *item_name;
-    int x, y;
-    int collected;
-    int damage;
+    private:
+        const char *item_name;
+        int x, y;
+        int collected;
+        int damage;
+    public:
+    Item(int x, int y, const char* item_name, int damage); // инициализация
+    void print_item(Item* item); // вывод характеристик оружия
 };
 
-
-// PLAYER
-void init_player(Player* player, int x, int y, int health, int speed, int damage, int inventory_space); // инициализация
-void print_player(const Player* player); // вывод игрока
-void move_player(Player* player, int dx, int dy); // перемещаем игрока
-void heal_player(Player* player, int heal); // лечим игрока
-bool player_is_dead(Player* player); // проверяем, умер ли игрок
-void damage_player(Player* player, int damage); // наносим урон игроку 
-
-// POTION
-void init_potion(Potion* potion, int x, int y, int health_restore); // инициализация
-void print_potion(const Potion* potion); // вывод зелья
-void use_potion(Player* player, Potion* potion);  // используем зелье
-
-// COIN
-void init_coin(Coin* coin, int x, int y); // инициализация
-void collect_coin(Player* player, Coin* coin); // собираем монетку
-void print_coin(const Coin* coin);// вывод монетки
-
-// INVENTORY    
-void init_inventory(Inventory* inventory, int space); // инициализация
-void print_inventory(Inventory* inventory); // вывод инвентаря
-int inventory_add_item(Inventory* inventory, Item* item); // добавляем предмет в инвентарь
-
-// MONSTER
-void init_monster(Monsters* monster, int x, int y, int damage, int health); // инициализация
-void print_monster(const Monsters* monster); // вывод характеристик монстра
-void monster_attack(Player* player, Monsters* monster); // монстр атакует
-void damage_monster(Monsters* monster, int amount); // наносим урон монстру
-// ITEM
-void init_item(Item* item, int x, int y, const char* item_name, int damage); // инициализация
-void print_item(Item* item); // вывод характеристик оружия
 
 //GAMEPLAY
 void battle_with_monster(Player* player, Monsters* monster, Inventory* inventory); // функция сражения с монстром 
